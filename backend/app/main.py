be44f9f2d5eda40d5238133bd9abc0a54c0e7841
bizhanchik <bizhanash@gmail.com>
2025-11-15 13:23:12 +0500

    Backend: route for predictions added

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,5 +7,23 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, World!"}
 
+#fake ml lol
+@app.post("/predict")
+async def predict(request: Request):
+    return {
+        "boxes": [
+            {
+                "x1": 50,
+                "y1": 40,
+                "x2": 200,
+                "y2": 300,
+                "confidence": 0.75,
+                "class": "object"
+            }
+        ],
+        "model_version": "mock_v1",
+        "latency_ms": 5
+    }
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
